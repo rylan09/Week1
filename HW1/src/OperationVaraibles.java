b1a525f09ebebd83ec19a5fde3f083d626f13ff6
rylan09 <43859165+rylan09@users.noreply.github.com>
2018-10-04 19:13:43 -0700

    Add files via upload

---
+++ HW1/src/OperationVaraibles.java
@@ -0,0 +1,43 @@
+
+public class OperationVaraibles {
+
+	public static void main(String[] args) {
+		//variable1
+		int weight = 223;
+		System.out.println(weight);
+		weight = weight - 68; 
+		System.out.println(weight);
+		
+		//variable2
+		int show = 25 * 7;
+		System.out.println(show);
+		
+		boolean isshow25 = show == 25;
+		System.out.println(isshow25);
+		
+		System.out.println(show == 175);
+		
+		show = show / 5;
+		System.out.println(show);
+		
+		//varible3
+		String who = "We";
+		String what = "Are";
+		String family = "One";
+		String Legion = who + what + family;
+		System.out.println(Legion);
+		
+		//varible4
+		String firstName = "Rylan";
+		String lastName = "Echalar";
+		String fullName = firstName + " " + lastName;
+		
+		//varible4
+		String myFirstName = "Rylan";
+		String myLastName = "Echalar";
+		String myFullName = firstName + " " + lastName;
+		System.out.println(fullName);
+		
+		
+	}
+}
---
+++ HW1/src/PrimitiveVariables.java
@@ -0,0 +1,34 @@
+
+public class PrimitiveVariables {
+
+	public static void main(String[] args) {
+		//variable1
+		int year = 12;
+		double hours = 97876.954;
+		char category = 'A';
+		boolean isNumberOne = false;
+		String nameSite = "Netflix";
+		
+		System.out.println(year);
+		System.out.println(hours);
+		System.out.println(category);
+		System.out.println(isNumberOne);
+		System.out.println(nameSite);
+		
+		//variable2
+		int dogsAge = 7;
+		double weight = 78.35;
+		char breedClass = 'K';
+		boolean isHeMean = false;
+		String dogsName = "Rocco";
+		
+		System.out.println(dogsAge);
+		System.out.println(weight);
+		System.out.println(breedClass);
+		System.out.println(isHeMean);
+		System.out.println(dogsName);
+		
+
+	}
+
+}
